7481d751313aa1d0620adfa883e7ed208e13bff8
vpopov <vpopov@tradedealer.ru>
2025-06-09 21:04:23 +0500

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,16 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: process.env.NODE_ENV === 'production' ? '/saa-soft/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
